9e43fac724b658424d2d1c97350cfea039e00f35
Jason Hall <hall.jasondaniel@gmail.com>
2017-05-09 16:29:12 -0600

    some styling on category show page

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,7 @@ class Item < ApplicationRecord
   validates_presence_of :title, :description, :price, :category_id
 
   belongs_to :category
+
+  mount_uploader :thumb_image, ImageUploader
+  mount_uploader :image, ImageUploader
 end
